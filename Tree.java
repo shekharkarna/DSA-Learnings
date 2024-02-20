4b6e14e41a33997936197db6218fd58719ac4123
Shekhar <betistata@gmail.com>
2024-02-20 22:06:17 +0530

    Tree: Tree Traversal Algos Pre,Post, In and level order

--- Tree.java
+++ Tree.java
@@ -1,6 +1,74 @@
+import java.util.LinkedList;
+
+/**
+/* Tree is a hierarchical data structure in which a collection of elements known as nodes are connected to each other via edges
+/* there exists exactly one path between any two nodes.
+/* Here example is of Binary Tree
+ */
 public class Tree {
 
-    public void show() {
-        System.out.println("inside show method for Tree Data Structure");
+    /** This will return a treeNode which is root of tree */
+    public TreeNode create() {
+        final TreeNode rootNode = new TreeNode(1);
+        final TreeNode leftChild = new TreeNode(2);
+        final TreeNode rightChild = new TreeNode(3);
+        final TreeNode leftLeftChild = new TreeNode(4);
+        final TreeNode leftRightChild = new TreeNode(5);
+        final TreeNode rightRightChild = new TreeNode(6);
+
+        rootNode.left = leftChild;
+        rootNode.right = rightChild;
+
+        leftChild.left = leftLeftChild;
+        leftChild.right = leftRightChild;
+
+        rightChild.right = rightRightChild;
+        return rootNode;
+    }
+
+    public void preOrderTraversal(final TreeNode treeNode) {
+        if(treeNode == null) {
+            return;
+        }
+        System.out.println(treeNode.data);
+        preOrderTraversal(treeNode.left);
+        preOrderTraversal(treeNode.right);
+    }
+
+    public void inOrderTraversal(final TreeNode treeNode) {
+        if(treeNode == null) {
+            return;
+        }
+        inOrderTraversal(treeNode.left);
+        System.out.println(treeNode.data);
+        inOrderTraversal(treeNode.right);
+    }
+
+    public void postOrderTraversal(final TreeNode treeNode) {
+        if(treeNode==null) {
+            return;
+        }
+        postOrderTraversal(treeNode.left);
+        postOrderTraversal(treeNode.right);
+        System.out.println(treeNode.data);
+    }
+
+    public void levelOrderTraversal(final TreeNode treeNode) {
+        LinkedList<TreeNode> linkedList = new LinkedList<>();
+        if(treeNode==null) {
+            return;
+        }
+        linkedList.add(treeNode);
+
+        while(!linkedList.isEmpty()) {
+            TreeNode node = linkedList.poll();
+            if(node.left != null) {
+                linkedList.add(node.left);
+            }
+            if(node.right != null) {
+                linkedList.add(node.right);
+            }
+            System.out.println(node.data);
+        }
     }
 }
--- TreeSolution.java
+++ TreeSolution.java
@@ -2,6 +2,15 @@ public class TreeSolution {
 
     public static void main(String[] args) {
         final Tree tree = new Tree();
-        tree.show();
+        TreeNode rootNode = tree.create();
+
+        System.out.println("printing PreOrder Traversal");
+        tree.preOrderTraversal(rootNode);
+        System.out.println("printing InOrder Traversal");
+        tree.inOrderTraversal(rootNode);
+        System.out.println("printing PostOrder Traversal");
+        tree.postOrderTraversal(rootNode);
+        System.out.println("printing Level Order Traversal");
+        tree.levelOrderTraversal(rootNode);
     }
 }
